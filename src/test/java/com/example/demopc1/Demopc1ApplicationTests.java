a5496b4dadd7f8ab6659c38f2f788ee7568fe35d
HP <mdzaid1299@gmail.com>
2022-11-09 00:49:56 +0530

    ini

--- src/test/java/com/example/demopc1/Demopc1ApplicationTests.java
+++ src/test/java/com/example/demopc1/Demopc1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demoMc1;
+package com.example.demopc1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoMc1ApplicationTests {
+class Demopc1ApplicationTests {
 
 	@Test
 	void contextLoads() {
